3405bf5498f62bf8b3d08669d69404e4da0c26d6
jacobthebusinessman <83357520+jacobthebuisnessman@users.noreply.github.com>
2024-01-20 22:12:17 +0000

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -3,16 +3,16 @@ print("""
 1 - rock
 2 - paper
 3 - scissors""")
-
+###################################
 def rand():
     bot = random.randrange(1,4)
     if bot == 1:
-        print("rock")
+        print("Bot Plays Rock")
     elif bot == 2:
-        print("paper")
+        print("Bot Plays Paper")
     else:
-        print("scissors")
-##################################
+        print("Bot Plays Scissors")
+###################################
     if bot == user:
         print("draw")
     elif user == 1 and bot == 3:
@@ -23,7 +23,18 @@ def rand():
         print("User Wins")
     else:
         print("Bot Wins")
-
+###################################
 user = int(input("input: "))
+a = 1
+while a < 50:
+    print("\n")
+    a = a + 1
+if user == 1:
+    print("You Play Rock")
+elif user == 2:
+    print("You Play Paper")
+else:
+    print("You Play Scissors")
+###################################
 rand()
 
