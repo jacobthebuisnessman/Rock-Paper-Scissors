b77436ad4b817c4962d24d26a42d3a47bbdece82
jacobthebusinessman <83357520+jacobthebuisnessman@users.noreply.github.com>
2024-01-20 19:40:54 +0000

    Create Rock-Paper-Scissors.py

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,21 @@
+import random
+print("""
+1 - rock
+2 - paper
+3 - scissors""")
+
+def rand():
+    a = random.randrange(1,4)
+    print(a)
+    if x == 3 and a == 2:
+        print("user wins")
+    elif x == 2 and a == 3:
+        print("user wins")
+    elif x == 1 and a == 1:
+        print("user wins")
+    else:
+        print("machine wins")
+
+x = int(input("input: "))
+rand()
+
